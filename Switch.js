a58ec8fde17d0ef8b51fa7c5cf331f4917a27427
bilel7 <31697833+bilel7@users.noreply.github.com>
2018-03-31 13:30:15 +0200

    les instruction conditionnelles

--- Switch.js
+++ Switch.js
@@ -1,10 +1,35 @@
-var quantity = number,
-    unit = String,
-    result = quantity * unit;
-
-
-
-if (quantity === 1 && unit === 'k'){
-    result= 1*1024
-    console.log(result)
+var quantity = 1,
+    unit = 'M',
+    result;
+if (unit === 'K') {
+    result = 1024 * quantity;
+} else if (unit === 'M') {
+    result = 1024 * 1024 * quantity;
+} else if (unit === 'G') {
+    result = 1024 * 1024 * 1024 * quantity;
+} else if (unit === 'T') {
+    result = 1024 * 1024 * 1024 * 1024 * quantity;
+} else {
+    result = false;
 }
+
+console.log(result);
+
+switch (unit) {
+    case 'K':
+        result = 1024 * quantity;
+        break;
+    case 'M':
+        result = 1024 * 1024 * quantity;
+        break;
+    case 'G':
+        result = 1024 * 1024 * 1024 * quantity;
+        break;
+    case 'T':
+        result = 1024 * 1024 * 1024 * 1024 * quantity;
+        break;
+    default:
+        result = false;
+}
+
+console.log(result);
